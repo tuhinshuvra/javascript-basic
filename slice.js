c08dd2ea41f5313773aa42d8ec3c645a5ca792a8
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-08-09 08:35:53 +0600

    devisable js

--- slice.js
+++ slice.js
@@ -2,10 +2,25 @@
 
 
 
-const student = { id: 121, class: 12, name: "Arjun" };
-const friendsAge = [23, 34, 32, 41, 25, 29, 27];
-const friendsName = ["Sachin", "Dheman", "Rahul", "Peyus"];
-const newFriendsName = ["Rashid", "Babor", "Akram", "Babul"];
+// const student = { id: 121, class: 12, name: "Arjun" };
+// const friendsAge = [23, 34, 32, 41, 25, 29, 27];
+// const friendsName = ["Sachin", , "Shourav", "Dheman", "Rahul", "Dheman", "Peyus", "Shourav", "Rahul", "Dheman", "Peyus",];
+// const newFriendsName = ["Rashid", "Babor", "Akram", "Babul", "Shahid"];
+// const newName = friendsName.slice(2, 5);
+// const newSplice = friendsName.splice(2, 5, "Bahadur", "Bikram");
+// console.log(newSplice);
+// console.log(friendsName);
 
-const newName = friendsName.slice(2, 5);
-console.log(newName);
+const friendsName = ["Sachin", "Shourav", "Dheman", "Rahul", "Dheman", "Peyus", "Shourav", "Rahul", "Dheman", "Peyus"];
+function removeDuplicate(friendsName) {
+    const unique = [];
+    for (let i = 0; i < friendsName.length; i++) {
+        const name = friendsName[i];
+        if (unique.includes(name) === false) {
+            unique.push(name);
+        }
+    }
+    return unique;
+}
+const uniqueName = removeDuplicate(friendsName);
+console.log(uniqueName);
